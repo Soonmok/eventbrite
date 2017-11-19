f3b4bfd198024f5155bf3aeae8c5741d85d65747
Soonmok <tnsahr2580@gmail.com>
2017-11-19 20:39:36 +0900

    added mlab url

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.locals.querystring = require('querystring');
 mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
 //const connStr = 'mongodb://localhost/mjdb3';
 // 아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
-const connStr = 'mongodb://dbuser1:mju12345@ds113825.mlab.com:13825/sampledb1';
+const connStr = "mongodb://soonmok:123123@ds113566.mlab.com:13566/mokdatabase";
 mongoose.connect(connStr, {useMongoClient: true });
 mongoose.connection.on('error', console.error);
 
