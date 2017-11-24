4de7090b1f8fc4854ecdf5efae4ab82f5a4dd9a1
tnsahr2580@gmail.com <tnsahr2580@gmail.com>
2017-11-24 22:43:06 +0900

    added kakao login function

--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,20 @@
 module.exports = (app, passport) => {
-    app.get('/signin', (req, res, next) => {
-      
-      res.render('signin');
-    });
-  
-    app.post('/signin', passport.authenticate('local-signin', {
-      successRedirect : '/', // redirect to the secure profile section
-      failureRedirect : '/signin', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-    }));
-  
-    app.get('/auth/facebook',
-      passport.authenticate('facebook', { scope : 'email' })
-    );
-  
-    app.get('/auth/facebook/callback',
-      passport.authenticate('facebook', {
-        failureRedirect : '/signin',
-        failureFlash : true // allow flash messages
-      }), (req, res, next) => {
-        req.flash('success', 'Welcome!');
-        res.redirect('/');
-      }
-    );
+  app.get('/signin', (req, res, next) => {
+    res.render('signin');
+  });
 
-    app.get('/auth/kakao',
-    passport.authenticate('kakao', { scope : 'email' })
+  app.post('/signin', passport.authenticate('local-signin', {
+    successRedirect : '/', // redirect to the secure profile section
+    failureRedirect : '/signin', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+  app.get('/auth/facebook',
+    passport.authenticate('facebook', { scope : 'email' })
   );
 
-  app.get('/auth/kakao/callback',
-    passport.authenticate('kakao', {
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
       failureRedirect : '/signin',
       failureFlash : true // allow flash messages
     }), (req, res, next) => {
@@ -37,18 +22,24 @@ module.exports = (app, passport) => {
       res.redirect('/');
     }
   );
+  app.get('/auth/kakao',
+    passport.authenticate('kakao')
+  );
 
-  
-    app.get('/signout', (req, res) => {
-      req.logout();
-      req.flash('success', 'Successfully signed out');
+  app.get('/auth/kakao/callback',
+    passport.authenticate('kakao', {
+      failureRedirect : '/signin',
+      failureFlash : true // allow flash messages
+    }), (req, res, next) => {
+      console.log("im in kakao");
+      req.flash('success', 'Welcome!');
       res.redirect('/');
-    });
+    }
+  );                                                                                                         
 
-    app.get('/edit', (req, res) => {
-      req.logout();
-      req.flash('success', 'Successfully signed out');
-      res.redirect('/');
-    });
-  };
-  
+  app.get('/signout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Successfully signed out');
+    res.redirect('/');
+  });
+};
