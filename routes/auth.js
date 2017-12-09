88f00451832a36c797798ccd23ff782c6601f5b0
tnsahr2580@gmail.com <tnsahr2580@gmail.com>
2017-12-09 15:08:03 +0900

    added password change email system

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,43 @@
 const catchErrors = require('../lib/async-error');
+const User = require('../models/user');
+
 
 module.exports = (app, passport) => {
   app.post('/forget-password', catchErrors(async (req, res, next) => {
-    var email = req.body.email;
-  }));
-
-  app.post('/reset', catchErrors(async (req, res, next) => {
-
+    var _email = req.body.email;
+    var nodemailer = require('nodemailer');
+    
+    const user = await User.findOne({email: _email});
+    console.log(user);
+    // create reusable transporter object using the default SMTP transport
+    var transporter = nodemailer.createTransport( {
+      host: "smtp.gmail.com", // hostname
+      secureConnection: true, // use SSL
+      port: 465, // port for secure SMTP
+      auth: {
+          user: "tnsahr2580@gmail.com",
+          pass: process.env.GMAIL_PASSWORD
+      }
+  });
+  
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: 'tnsahr2580@gmail.com', // sender address
+        to: _email, // list of receivers
+        subject: 'Change Your Password -MoksBrite', // Subject line
+        text: 'asdasd', // plaintext body
+        html: `<a href="http://localhost:3000/${user.id}/reset">change your password</a>` // html body
+    };
+    console.log("this is html");
+    console.log(mailOptions.html);
+    //send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+    res.redirect('/waiting');
   }));
 
   app.get('/signin', (req, res, next) => {
@@ -22,10 +53,6 @@ module.exports = (app, passport) => {
   app.get('/forget-password', (req, res, next) => {
     res.render('forget-password');
   });
-
-  app.post('/reset-password', catchErrors(async (req, res, next) => {
-    res.render('/');
-  }));
 
   app.get('/auth/facebook',
     passport.authenticate('facebook', { scope : 'email' })
@@ -49,7 +76,6 @@ module.exports = (app, passport) => {
       failureRedirect : '/signin',
       failureFlash : true // allow flash messages
     }), (req, res, next) => {
-      console.log("im in kakao");
       req.flash('success', 'Welcome!');
       res.redirect('/');
     }
@@ -60,4 +86,17 @@ module.exports = (app, passport) => {
     req.flash('success', 'Successfully signed out');
     res.redirect('/');
   });
+
+  app.get('/:id/reset', catchErrors(async (req, res, next) => {
+    res.render('reset', {user :req.params.id});
+  }));
+  
+  app.post('/:id/reset', catchErrors(async (req, res, next) => {
+    const user = await User.findOne({_id: req.params.id});
+    user.password = await user.generateHash(req.body.password);
+    await user.save();
+    req.flash('success', 'Updated successfully.');
+    res.redirect('/');
+  }));
+  
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.get('/waiting', function(req, res, next) {
+  res.render('waiting');
+});
+
 module.exports = router;
